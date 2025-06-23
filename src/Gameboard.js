e77072c21f2cb10df6a5faeaba65c96849e0a195
Miran <miranabdelaziz@yahoo.com>
2025-06-22 23:02:43 -0400

    Implemented a feature that ensures all ships are 1 grid apart horizontally and vertically

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,4 +1,4 @@
-class Gameboard {
+export default class Gameboard {
     constructor() {
         this.grid = [];
         for (let i = 0; i < 10; i++) {
@@ -9,33 +9,74 @@ class Gameboard {
             this.grid.push(row);
         }
     }
+
     receiveAttack(x, y) {
         this.grid[x][y].isHit = true;
     }
+
     placeShip(Ship, [initialRow, initialColumn]) {
-        try {
-            let isGridOccupied = false;
-            for (let i = 0; i < Ship.length; i++) {
-                if (Ship.isVertical) {
-                    // Throw error if grid is already occupiedAdd commentMore actions
-                    isGridOccupied = this.grid[initialRow + i][initialColumn].ship !== null;
-                    if (isGridOccupied) {
-                        throw new Error('Invalid coordinates');
-                    }
-                    this.grid[initialRow + i][initialColumn].ship = Ship;
-                } else {
-                    // Throw error if grid is already occupied
-                    isGridOccupied = this.grid[initialRow][initialColumn + i].ship !== null;
-                    if (isGridOccupied) {
-                        throw new Error('Invalid coordinates');
-                    }
-                    this.grid[initialRow][initialColumn + i].ship = Ship;
-                }
+        let isGridOccupied = false;
+        let isGridOutOfBounds = false;
+        let currentRow;
+        let currentColumn;
+        for (let i = 0; i < Ship.length; i++) {
+            if (Ship.isVertical) {
+                currentRow = initialRow + i;
+                currentColumn = initialColumn;
+            } else {
+                currentRow = initialRow;
+                currentColumn = initialColumn + i;
             }
-        } catch (error) {
-            throw new Error('Invalid coordinates');
+
+            // Throw error if grid is out of bounds.
+            isGridOutOfBounds = currentRow > 9 || currentColumn > 9;
+            if (isGridOutOfBounds) {
+                throw new Error('Invalid coordinates: Out of bounds');
+            }
+
+            // Throw error if grid is already occupied
+            isGridOccupied = this.grid[currentRow][currentColumn].ship !== null;
+            if (isGridOccupied) {
+                throw new Error('Invalid coordinates: Grid already occupied');
+            }
+
+            // Get all possible adjacent coordinates of current grid.
+            let adjacentGridsCoordinates = [
+                [currentRow - 1, currentColumn - 1],
+                [currentRow - 1, currentColumn],
+                [currentRow - 1, currentColumn + 1],
+                [currentRow, currentColumn - 1],
+                [currentRow, currentColumn + 1],
+                [currentRow + 1, currentColumn - 1],
+                [currentRow + 1, currentColumn],
+                [currentRow + 1, currentColumn + 1],
+            ];
+
+            // Get all the valid coordinates (not out of bounds).
+            adjacentGridsCoordinates = adjacentGridsCoordinates.filter(([gridCoordinateX, gridCoordinateY]) => {
+                if (gridCoordinateX >= 0 && gridCoordinateX <= 9 && gridCoordinateY >= 0 && gridCoordinateY <= 9) {
+                    return true;
+                }
+                return false;
+            });
+
+            // Get all the valid grid from the valid coordinates.
+            let adjacentGrids = adjacentGridsCoordinates.map(([adjacentGridsCoordinatesX, adjacentGridsCoordinatesY]) => {
+                return this.grid[adjacentGridsCoordinatesX][adjacentGridsCoordinatesY];
+            });
+
+            // Check if all valid adjacent grids are empty or occupied by the same ship to be placed at the current grid.
+            let isShipOneGridApart = adjacentGrids.every((grid) => {
+                return grid.ship === null || grid.ship === Ship;
+            });
+
+            // Throw error if there is an adjacent grid that is occupied by a different ship (Ships are not 1-grid apart).
+            if (!isShipOneGridApart) {
+                throw new Error('Invalid coordinates: Ships are not 1-grid apart');
+            }
+
+            // Place the ship at current grid.
+            this.grid[currentRow][currentColumn].ship = Ship;
         }
     }
 }
-
-export default Gameboard
